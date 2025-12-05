15fe524cdb1fe919eae8e19382f9bcd1bf5bd078
weiwenjian <3194603353@qq.com>
2025-12-06 02:51:12 +0800

    111

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import FinanceManagement from "../views/finance-management.vue";
 import Cart from '@/views/cart.vue'
 import ProductDetail from '@/views/product-detail.vue'
@@ -21,7 +21,7 @@ function roleHomePath(role: string | null): string {
 }
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
